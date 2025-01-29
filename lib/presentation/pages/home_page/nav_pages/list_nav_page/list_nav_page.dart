2cd7de26f4fe54e0f73308513659e394501bd9f9
Alexander Aks'onov <m.mirvua@gmail.com>
2025-01-29 13:21:24 +0200

    working on the list tile for the list;

--- lib/presentation/pages/home_page/nav_pages/list_nav_page/list_nav_page.dart
+++ lib/presentation/pages/home_page/nav_pages/list_nav_page/list_nav_page.dart
@@ -1,9 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:harry_potter_sorting_flutter/data/database/database_schema.dart';
 import 'package:harry_potter_sorting_flutter/data/network/dio_client.dart';
 import 'package:harry_potter_sorting_flutter/data/repositories/character_repository_impl.dart';
 import 'package:harry_potter_sorting_flutter/presentation/pages/home_page/nav_pages/list_nav_page/widgets/status_icon.dart';
 import 'package:harry_potter_sorting_flutter/presentation/common/widgets/character_photo.dart';
+import 'package:harry_potter_sorting_flutter/router/router.dart';
 
 class ListNavPage extends StatefulWidget {
   const ListNavPage({super.key});
@@ -61,61 +63,75 @@ class _ListNavPageState extends State<ListNavPage> with WidgetsBindingObserver {
 
                   final character = entries[index];
 
-                  return Row(
-                    children: [
+                  return InkWell(
+                    onTap: () {
+                      context.router.push(DetailRoute(name: entries[index].name));
+                    },
+                    child: Row(
+                      children: [
 
-                      CharacterPhoto(
-                        width: 35,
-                        height: 50,
-                        borderRadius: 2.0,
-                        imageSrc: character.imageSrc,
-                        smallIconSize: 20,
-                      ),
+                        CharacterPhoto(
+                          width: 35,
+                          height: 50,
+                          borderRadius: 2.0,
+                          imageSrc: character.imageSrc,
+                          smallIconSize: 20,
+                        ),
 
-                      const SizedBox(width: 12.0,),
+                        const SizedBox(width: 12.0,),
 
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
 
-                          Text(
-                            character.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              character.name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
 
-                          Text(
-                            'Attempts: ${character.totalCount}',
-                            style: const TextStyle(
-                              fontSize: 12.0
+                            Text(
+                              'Attempts: ${character.totalCount}',
+                              style: const TextStyle(
+                                fontSize: 12.0
+                              ),
                             ),
-                          ),
 
-                        ],
-                      ),
+                          ],
+                        ),
 
-                      const Spacer(),
+                        const Spacer(),
 
-                      //show success icon if there is 1 successful attempt
-                      if (character.successCount > 0)
-                        const StatusIcon(icon: Icons.check, backgroundColor: Colors.green),
+                        //show success icon if there is 1 successful attempt
+                        if (character.successCount > 0)
+                          const StatusIcon(icon: Icons.check, backgroundColor: Colors.green),
 
-                      Row(
-                        spacing: 12.0,
-                        children: [
+                        Row(
+                          spacing: 12.0,
+                          children: [
 
-                          //show retry button if no success recorded
-                          if (character.successCount == 0)
-                            const StatusIcon(icon: Icons.refresh, backgroundColor: Colors.grey),
+                            //show retry button if no success recorded
+                            if (character.successCount == 0)
+                              StatusIcon(
+                                icon: Icons.refresh,
+                                backgroundColor: Colors.grey,
+                                onTap: () {
+                                  // todo: open home page with the selected character
+                                },
+                              ),
 
-                          //show failure icon if attempts were made, but 0 successful;
-                          if (character.totalCount > 0 && character.successCount == 0)
-                            const StatusIcon(icon: Icons.close, backgroundColor: Colors.red),
-                        ],
-                      ),
+                            //show failure icon if attempts were made, but 0 successful;
+                            if (character.totalCount > 0 && character.successCount == 0)
+                              const StatusIcon(
+                                icon: Icons.close,
+                                backgroundColor: Colors.red,
+                              ),
+                          ],
+                        ),
 
-                    ],
+                      ],
+                    ),
                   );
 
                 },
--- lib/presentation/pages/home_page/nav_pages/list_nav_page/widgets/status_icon.dart
+++ lib/presentation/pages/home_page/nav_pages/list_nav_page/widgets/status_icon.dart
@@ -4,26 +4,31 @@ class StatusIcon extends StatelessWidget {
   
   final IconData icon;
   final Color backgroundColor;
+  final Function()? onTap;
   
   const StatusIcon({
     required this.icon,
     required this.backgroundColor,
+    this.onTap,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        shape: BoxShape.circle,
-      ),
-      child: Icon(
-        icon,
-        color: Colors.white,
-        size: 24.0,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          icon,
+          color: Colors.white,
+          size: 24.0,
+        ),
       ),
     );
   }
