d0ec31206e5d4173a8ac93faff548425bcc54f9c
Alexander Aks'onov <m.mirvua@gmail.com>
2025-01-30 16:39:33 +0200

    clean up;

--- lib/presentation/pages/home_page/nav_pages/home_nav_page/notifiers/character_notifier.dart
+++ lib/presentation/pages/home_page/nav_pages/home_nav_page/notifiers/character_notifier.dart
@@ -3,21 +3,9 @@ import 'package:harry_potter_sorting_flutter/data/database/database_schema.dart'
 import 'package:harry_potter_sorting_flutter/domain/models/character_dto.dart';
 
 class CharacterNotifier extends ChangeNotifier {
-  Character? _selectedCharacter;
-  Character? get selectedCharacter => _selectedCharacter;
 
   CharacterDTO? _character;
   CharacterDTO? get character => _character;
-
-  void selectCharacter(Character? character) {
-    _selectedCharacter = character;
-    notifyListeners();
-  }
-
-  void clearSelection() {
-    _selectedCharacter = null;
-    notifyListeners();
-  }
 
   void updateCharacter(CharacterDTO character) {
     _character = character;
