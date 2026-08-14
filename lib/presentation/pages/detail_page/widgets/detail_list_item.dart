9cc81e46eb3a09c636f07bb30fc4324d5fa08502
Alexander Aks'onov <m.mirvua@gmail.com>
2026-08-14 12:25:41 +0300

    minor improvements to details page;

--- lib/presentation/pages/detail_page/widgets/detail_list_item.dart
+++ lib/presentation/pages/detail_page/widgets/detail_list_item.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-class DetailListItem extends StatelessWidget {
+import '../../../style/app_colors.dart';
 
+class DetailListItem extends StatelessWidget {
   final String label;
   final String value;
 
@@ -13,18 +14,18 @@ class DetailListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Row(
       spacing: 8.0,
       children: [
         Text(
           label,
-          style: const TextStyle(fontWeight: FontWeight.w500),
+          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
         ),
-
-        Text(value.isNotEmpty ? value : ' - '),
+        Text(
+          value.isNotEmpty ? value : ' - ',
+          style: const TextStyle(fontSize: 18, color: AppColors.deepGold),
+        ),
       ],
     );
-
   }
 }
