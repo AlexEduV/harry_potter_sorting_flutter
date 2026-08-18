ea73d09e9df1fdcfdecfec88ccb618470882780b
Alexander Aks'onov <m.mirvua@gmail.com>
2026-08-18 10:01:30 +0300

    added base log interceptor;

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -3,24 +3,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:harry_potter_sorting_flutter/common/constants/api_constants.dart';
 
 class DioClient {
-  static final Dio _client = Dio(BaseOptions(baseUrl: ApiConstants.baseUrl));
+  static final Dio _client = _createClient();
 
   static Dio client() => _client;
 
-  static String handleError(DioException e) {
-    // Log the exception for debugging purposes
-    debugPrint('Error: ${e.toString()}');
+  static Dio _createClient() {
+    final dio = Dio(BaseOptions(baseUrl: ApiConstants.baseUrl));
 
-    // Handle known errors
-    if (e.type == DioExceptionType.connectionTimeout) {
-      return 'Connection Error - Timeout';
-    } else if (e.type == DioExceptionType.badResponse) {
-      return 'Server Error. Please, try again';
-    } else if (e.type == DioExceptionType.cancel) {
-      return 'Request was canceled';
-    }
+    dio.interceptors.add(
+      LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+        error: true,
+        logPrint: (log) => debugPrint(log.toString()),
+      ),
+    );
 
-    // Fallback for unknown errors
-    return 'Unknown Error: Please try again later';
+    return dio;
   }
 }
