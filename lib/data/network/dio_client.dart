34054416ecb38092118faaebbb08d0ff31ed107c
Alexander Aks'onov <m.mirvua@gmail.com>
2026-08-18 09:36:42 +0300

    reworking api;

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -1,8 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:harry_potter_sorting_flutter/common/constants/api_constants.dart';
 
 class DioClient {
-  static final Dio _client = Dio();
+  static final Dio _client = Dio(BaseOptions(baseUrl: ApiConstants.baseUrl));
 
   static Dio client() => _client;
 
